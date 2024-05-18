749898f0430141e10cba26e207f760137ae3f613
unknown <twitchnmh@gmail.com>
2024-05-18 20:53:02 +0200

    initial commit

---
+++ src/main/java/com/glasslegion/glasslegion/GlassLegionApplication.java
@@ -0,0 +1,13 @@
+package com.glasslegion.glasslegion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassLegionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassLegionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glasslegion/glasslegion/GlassLegionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glasslegion.glasslegion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassLegionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
